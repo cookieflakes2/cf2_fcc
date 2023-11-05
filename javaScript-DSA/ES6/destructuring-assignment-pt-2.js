5424f290e000bacb5ce487ae010e29d65a6092c8
Allan Murungi <leomacowski@gmail.com>
2023-11-05 22:26:34 +0800

    Use Destructuring Assignment to Assign Variables to Objects

---
+++ javaScript-DSA/ES6/destructuring-assignment-pt-2.js
@@ -0,0 +1,16 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
+
+/*
+Destructuring allows you to assign a new variable name when extracting values. 
+You can do this by putting the new name after a colon when assigning the value.
+*/
