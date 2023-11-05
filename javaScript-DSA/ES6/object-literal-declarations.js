db7e20ae083f6524f63e2cc6fea621258b4e7a0e
Allan <allancheboiw0@gmail.com>
2023-11-06 05:01:07 +0800

    Object Literal Declarations Using Object Property

---
+++ javaScript-DSA/ES6/object-literal-declarations.js
@@ -0,0 +1,16 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    /*
+      **INITIAL CODE**
+        return {
+          name: name,
+          age: age,
+          gender: gender
+        }
+     */
+  
+    return ({ name, age, gender });
+    // Only change code above this line
+  };
