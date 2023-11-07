20da80d04924cc13225d68abbfb510ce4e206d23
Allan <allancheboiw0@gmail.com>
2023-11-08 07:28:03 +0800

    Ignoring Case While Matching String

---
+++ javaScript-DSA/Regex/ignoring-case.js
@@ -0,0 +1,12 @@
+// Ignore Case While Matching
+
+/*
+You can match both uppercase and lowercase using what is called a flag. There are other flags but here you'll focus on the flag that ignores case - the i flag. You can use it by appending it to the regex. An example of using this flag is /ignorecase/i. This regex can match the strings ignorecase, igNoreCase, and IgnoreCase.
+*/
+
+// EXERCISE
+// Write a regex fccRegex to match freeCodeCamp, no matter its case. Your regex should not match any abbreviations or variations with spaces.
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
