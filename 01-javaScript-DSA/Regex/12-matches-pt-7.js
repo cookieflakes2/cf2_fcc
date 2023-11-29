417a26d4a9579779817d72ac4d994ac0885f9d3d
Allan <allancheboiw0@gmail.com>
2023-11-29 17:35:38 +0800

    Matching Single Characters Not Specified

---
+++ 01-javaScript-DSA/Regex/12-matches-pt-7.js
@@ -0,0 +1,5 @@
+// Matching Single Characters Not Specified
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
